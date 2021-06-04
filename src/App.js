ac0182ff526bcf42b555efaeebc5439e043dbec4
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-06-04 15:51:40 -0500

    Display map with markers based on JSON data in the data/chicago-parks.json file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
-import './App.css';
+import React, { useState } from 'react';
+import ReactMapGL, {Marker} from 'react-map-gl';
+import data from './data/chicago-parks.json';
 
 function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 41.8781,
+    longitude: -87.6298,
+    width: '100vw',
+    height: '100vh',
+    zoom: 10
+  })
+
+  const displayMarkers = () => {
+    return data.map((park) => {
+      if (park.location.hasOwnProperty('latitude') && park.location.hasOwnProperty('longitude')) {
+        const lat = parseFloat(park.location.latitude)
+        const lon = parseFloat(park.location.longitude)
+        return (
+          <Marker 
+            latitude={lat}
+            longitude={lon}
+          >
+            Park
+          </Marker>
+        )
+      } else {
+        return <></>
+      }
+      })
+
+  }
+
+
+
   return (
     <div className="App">
-        Map Test
+ 
+      <ReactMapGL 
+          {...viewport}
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+          mapStyle="mapbox://styles/mpuchyr/ckpiqwzd20yft17o9ibej6lic"
+          onViewportChange={(viewport) => {
+            setViewport(viewport)
+          }}
+        >
+          {displayMarkers()}
+        </ReactMapGL>
     </div>
   );
 }
